04b01c71f2b6de760dd194645cf8c8753bffb664
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2018-06-28 23:36:28 +0200

    Add files via upload

---
+++ Adventure 3/1/double.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+window = tk.Tk()
+
+def changeString():
+    stringToCopy = entry.get()
+    entry.insert(0, stringToCopy)
+
+entry = tk.Entry(window)
+button = tk.Button(window, text="Change", command=changeString)
+
+entry.pack()
+button.pack()
+window.mainloop()
